c2bc03784698a82496532d56af19ef7fceb4078f
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-10-16 15:01:05 -0300

    Created class EVENTO e enum with type the data

---
+++ src/main/java/br/com/alura/giovanni/codechella/Evento.java
@@ -0,0 +1,54 @@
+package br.com.alura.giovanni.codechella;
+
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDate;
+
+@Table("eventos")
+public class Evento {
+    private Long id;
+    private TipoEvento tipo;
+    private String nome;
+    private LocalDate data;
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TipoEvento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEvento tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/br/com/alura/giovanni/codechella/TipoEvento.java
@@ -0,0 +1,9 @@
+package br.com.alura.giovanni.codechella;
+
+public enum TipoEvento {
+    SHOW,
+    CONCERTO,
+    TEATRO,
+    PALESTRA,
+    WORKSHOP
+}
