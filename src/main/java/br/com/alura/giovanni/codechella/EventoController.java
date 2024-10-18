d4a6467084343a26c60b384a57df5dfe6737bc34
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-10-18 10:56:20 -0300

    Created and mapping 2 new function to create and delete registers

--- src/main/java/br/com/alura/giovanni/codechella/EventoController.java
+++ src/main/java/br/com/alura/giovanni/codechella/EventoController.java
@@ -2,10 +2,7 @@ package br.com.alura.giovanni.codechella;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,5 +22,15 @@ public class EventoController {
     @GetMapping("/{id}")
     public Mono<EventoDto> obterPorId(@PathVariable Long id){
         return servico.obterPorId(id);
+    }
+
+    @PostMapping
+    public Mono<EventoDto> cadastrar(@RequestBody EventoDto dto){
+        return servico.cadastrar(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> excluir(@PathVariable Long id){
+        return servico.excluir(id);
     }
 }
