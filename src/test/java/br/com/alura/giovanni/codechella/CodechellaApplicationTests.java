03b0fea412f6d8d909692ac37ff0301af0923e34
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-12-10 23:39:18 -0300

    Created a test to create event and refact the controller

--- src/test/java/br/com/alura/giovanni/codechella/CodechellaApplicationTests.java
+++ src/test/java/br/com/alura/giovanni/codechella/CodechellaApplicationTests.java
@@ -1,13 +1,38 @@
 package br.com.alura.giovanni.codechella;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)//qualquer porta pra fazer os testes
 class CodechellaApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void cadastraNovoEvento() {
+		//Cria o evento
+		EventoDto dto = new EventoDto(null, TipoEvento.SHOW, "Kiss",
+				LocalDate.parse("2025-01-01"),"Show da melhor banda que existe");
+
+		webTestClient.post().uri("/eventos").bodyValue(dto)//passando um post pra rodar na url eventos e passando como body o dto
+				.exchange() //envia a requisicao
+				.expectStatus().isCreated() // o que esperamos de status
+				.expectBody(EventoDto.class)//o que esperamos retornar
+				.value( response -> {
+					assertNotNull(response.id());
+					assertEquals(dto.tipo(), response.tipo());
+					assertEquals(dto.nome(), response.nome());
+					assertEquals(dto.data(), response.data());
+					assertEquals(dto.descricao(), response.descricao());
+				});
 	}
 
 }
